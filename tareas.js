e64d21b42c43394b11a8ebc9f93372d29102593c
keymg236 <queirolomg@gmail.com>
2021-08-07 14:06:05 -0300

    Descriptive message

---
+++ tareas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const tasks = [
+    {id: 1, name: "Tarea 1", completed: false, assigned: "Carolos"},
+    {id: 2, name: "Tarea 2", completed: false, assigned: "Bob"},
+    {id: 3, name: "Tarea 3", completed: false, assigned: "Zach"},
+
+];
+
+function todoList()
